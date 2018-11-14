8dd771060358910589a491e1e06df05c0e5672aa
乱心架刑 <c090817@users.noreply.github.com>
2018-11-14 15:18:49 +0800

    增加实体类

---
+++ bokeTest/src/com/bean/Posts.java
@@ -0,0 +1,53 @@
+package com.bean;
+
+import java.util.Date;
+
+public class Posts {
+
+	
+	//帖子
+	Integer id;
+	String title;
+	String content;
+	Date date;
+	Integer publishId;
+	String publishName;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public Integer getPublishId() {
+		return publishId;
+	}
+	public void setPublishId(Integer publishId) {
+		this.publishId = publishId;
+	}
+	public String getPublishName() {
+		return publishName;
+	}
+	public void setPublishName(String publishName) {
+		this.publishName = publishName;
+	}
+	
+	
+}
